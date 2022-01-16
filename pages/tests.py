7909f87471a4d38f42d58b1a6a98eafbf5fe1964
antheiz <salupacode@gmail.com>
2022-01-17 01:17:18 +0900

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,9 @@
+from urllib import response
+from django.test import SimpleTestCase
+
+# Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
